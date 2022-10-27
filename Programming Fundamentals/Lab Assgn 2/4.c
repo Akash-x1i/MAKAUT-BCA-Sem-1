8197b797112d221560b73a4439523979fed0d124
Akash Bauri <108396971+Akash-x1i@users.noreply.github.com>
2022-10-27 15:50:40 +0530

    centimetre data type changed

--- Programming Fundamentals/Lab Assgn 2/4.c
+++ Programming Fundamentals/Lab Assgn 2/4.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 void main(){
-    float cm, mtr, km;
+    float mtr, km;
+    int cm;
     printf("Enter length in Centimetre : \n");
-    scanf("%f",&cm);
+    scanf("%d",&cm);
 
     mtr=cm/100;
     km=mtr/1000;
 
-    printf("%f centimetre in meter is %.2f meter\n",cm ,mtr);
-    printf("%f centimetre in kilometre is %.3f kilometre",cm,km);
+    printf("%d centimetre in meter is %.2f meter\n",cm ,mtr);
+    printf("%d centimetre in kilometre is %.3f kilometre",cm,km);
 } 
