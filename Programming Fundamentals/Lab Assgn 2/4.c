cb44b5dafccf6fd330359cad6c27bd139b4db072
Akash Bauri <108396971+Akash-x1i@users.noreply.github.com>
2022-10-27 15:37:59 +0530

    Q4 of Lab Assignment 2 added

---
+++ Programming Fundamentals/Lab Assgn 2/4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    float cm, mtr, km;
+    printf("Enter length in Centimetre : \n");
+    scanf("%f",&cm);
+
+    mtr=cm/100;
+    km=mtr/1000;
+
+    printf("%f centimetre in meter is %.2f meter\n",cm ,mt);
+    printf("%f centimetre in kilometre is %.3f kilometre",k);
+} 
